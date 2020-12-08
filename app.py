0586341779a0c7d6abe1de20e336121d6eebb523
Jonathan Crecencio <jonathancrecencio@outlook.com>
2020-12-08 06:43:26 -0300

    Criando robô para envio de mensagem personalizada

---
+++ app.py
@@ -0,0 +1,33 @@
+#importar as bibliotecas
+from selenium import webdriver
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+
+#navegar ate o whatsapp web
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('https://web.whatsapp.com/')
+time.sleep(15)
+
+#definir contatos e grupos que serão enviados
+contatos = ['Contatos aqui dentro separando por aspas simples e virgulas']
+
+mensagem = 'Oi pessoal, desculpem mandar mensagem essa hora, estou testando automação, então quem está enviando é o robô que eu fiz rsrs (tô falando sério)'
+
+#buscar contatos / grupos
+def buscar_contato(contato):
+    campo_pesquisa = driver.find_element_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
+    time.sleep(4)
+    campo_pesquisa.click()
+    campo_pesquisa.send_keys(contato)
+    campo_pesquisa.send_keys(Keys.ENTER)
+
+def enviar_mensagem(mensagem):
+    campo_mensagem = driver.find_elements_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
+    campo_mensagem[1].click()
+    campo_mensagem[1].send_keys(mensagem)
+    campo_mensagem[1].send_keys(Keys.ENTER)
+
+for contato in contatos:
+    buscar_contato(contato)
+    enviar_mensagem(mensagem)
